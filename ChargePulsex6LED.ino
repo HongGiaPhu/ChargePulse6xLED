b5bb7a74a26f85a459420a4100302a2fbfa17c9b
HongGiaPhu <phumc0123@gmail.com>
2026-02-10 01:23:45 +0700

    Code and Tutorial

--- ChargePulsex6LED.ino
+++ ChargePulsex6LED.ino
@@ -1,40 +1,40 @@
-int LED[6] = {3,5,6,9,10,11}; //PWM pin
+int LED[6] = {3,5,6,9,10,11}; // PWM pin
 int button = 2; 
-int alert = 13; //Green Led
-int ready = 7; //Buzzer
+int alert = 13; // Green Led
+int ready = 7; // Buzzer
 
 void setup() {
   for(int i = 0; i < 6; i++) {
-    pinMode(LED[i], OUTPUT); //Set LED pins as output
+    pinMode(LED[i], OUTPUT); // Set LED pins as output
   }
-  pinMode(button, INPUT_PULLUP); //Easier wiring, reads LOW when pressed
+  pinMode(button, INPUT_PULLUP); / /Easier wiring, reads LOW when pressed
   pinMode(alert,OUTPUT); 
   pinMode(ready,OUTPUT);
 }
 
-int phase = 16; //when phase = 16, the LED brightness increases in 16 steps instead of 255
-bool state = false; //Logical state indicating the "fully charged" condition
+int phase = 16; // when phase = 16, the LED brightness increases in 16 steps instead of 255
+bool state = false; // Logical state indicating the "fully charged" condition
 
 void charging() { 
-  for(int k = 0; k <= 255; k += 256/phase) { //Increment brightness based on the selected phase
-    if (digitalRead(button) == HIGH)  //Only charge when the button is pressed. If not pressed (HIGH STATE), turn off LEDs
+  for(int k = 0; k <= 255; k += 256/phase) { // Increment brightness based on the selected phase
+    if (digitalRead(button) == HIGH)  // Only charge when the button is pressed. If not pressed (HIGH STATE), turn off LEDs
     {
       for(int i = 0;i<6;i++)
         digitalWrite(LED[i],LOW);
       return; //return immediately
     }
 
-    for(int i = 0; i < 6; i++) //Otherwise, increase LED brightness according to the current phase
+    for(int i = 0; i < 6; i++) // Otherwise, increase LED brightness according to the current phase
       analogWrite(LED[i], k); 
 
     delay(100); // Delay between steps (adjust for faster or slower charging)
   }
 
   state = true; // Charging completed
-  digitalWrite(alert, HIGH); //Buzzer beep
-  digitalWrite(ready,HIGH); //Green LED light up
+  digitalWrite(alert, HIGH); // Buzzer beep
+  digitalWrite(ready,HIGH); // Green LED light up
   delay(500);
-  digitalWrite(alert, LOW); //Buzzer off after 0.5 secs
+  digitalWrite(alert, LOW); // Buzzer off after 0.5 secs
 }
 
 
@@ -47,7 +47,7 @@ void pulse() {
     digitalWrite(LED[i], LOW); // Turn off LEDs one by one for a visual effect
     delay(50);
   }
-  state = false; //Reset charging state
+  state = false; // Reset charging state
 }
 
 void loop() {
